88e435b51f06fa625d434c05cf77998875c95844
Acfun <Acfun@gmail.com>
2015-12-01 10:06:06 +0800

    啦啦啦

---
+++ FFGit/FFGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFGit
+//
+//  Created by tarena02_04 on 15/12/1.
+//  Copyright © 2015年 tarena02_04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFGit/FFGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFGit
+//
+//  Created by tarena02_04 on 15/12/1.
+//  Copyright © 2015年 tarena02_04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
